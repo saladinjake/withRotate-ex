6d8f7bee4ce0dff2a30b131fb25b4114fb42c187
juwavictor <victor@sloovi.com>
2024-09-16 19:37:03 +0100

    chore: add fileupload and table component

--- src/components/file-uploader.tsx
+++ src/components/file-uploader.tsx
@@ -1,3 +1,105 @@
-const sample =() =>{
+import { useCallback } from "react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { useDropzone } from "react-dropzone";
+import Image from "next/image";
 
+
+interface IconProps {
+  name: string;
+  className?: string;
+  style?: any;
 }
+
+interface IUploadProps {
+  imageUrl?: string;
+  onBase64String: (data: string | ArrayBuffer | null) => void;
+}
+
+const Icon = (props: IconProps) => {
+  const { name, className, style = "" } = props;
+
+  return (
+    <svg viewBox="0 0 24 24" className={className} style={style}>
+      <use xlinkHref={`/icons/sprite.svg#${name}`} />
+    </svg>
+  );
+};
+
+const ImageTemplateBlank = () => {
+  return (
+    <Box position="relative">
+      <Icon
+        name="upload-clip"
+        style={{
+          fill: "none",
+          width: 20,
+          position: "absolute",
+          right: 0,
+        }}
+      />
+      <Icon name="upload" style={{ fill: "none", width: 64 }} />
+    </Box>
+  );
+};
+
+
+
+function parseUploadFiles(
+  file: File,
+  callback: (value: string | ArrayBuffer | null) => void
+) {
+  var reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onload = function () {
+    callback(reader.result);
+  };
+  reader.onerror = function (error) {
+    console.log("Error: ", error);
+  };
+}
+
+const FileUploader = (props: IUploadProps) => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    parseUploadFiles(acceptedFiles[0], (value) => {
+      props.onBase64String(value);
+    });
+
+    // Do something with the files
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <Box
+      border="1px solid rgba(94, 109, 250, 0.3)"
+      rounded="8px"
+      width="50%"
+      px="5"
+      py="4"
+      mb="7"
+      {...getRootProps()}
+    >
+      <input style={{ display: "none" }} {...getInputProps()} />
+      <Flex alignItems="center" columnGap="2.5">
+        {!props.imageUrl ? (
+          <ImageTemplateBlank />
+        ) : (
+          <Box height="64px" width="64px" position="relative" rounded="full">
+            <Image
+              src={props.imageUrl}
+              alt="company logo"
+              style={{ borderRadius: "100%" }}
+              fill
+            />
+          </Box>
+        )}
+
+        <Text fontSize={10} color="#5E6DFA">
+          Upload company logo
+        </Text>
+      </Flex>
+    </Box>
+  );
+};
+
+export default FileUploader;
--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,1 +1,104 @@
-const table = () => {}
+"use client";
+import {
+  getCoreRowModel,
+  ColumnDef,
+  useReactTable,
+  flexRender,
+} from "@tanstack/react-table";
+import { Box, Flex } from "@chakra-ui/react";
+import React from "react";
+
+interface ITableProps<C, D> {
+  data: D;
+  columns: C;
+}
+
+interface IconProps {
+  name: string;
+  className?: string;
+  style?: any;
+}
+
+const Icon = (props:  IconProps) => {
+  const { name, className, style = "" } = props;
+  return (
+    <svg viewBox="0 0 24 24" className={className} style={style}>
+      <use xlinkHref={`/icons/sprite.svg#${name}`} />
+    </svg>
+  );
+};
+
+
+
+const Table = <C extends ColumnDef<unknown, any>[], D extends unknown[]>(
+  props: ITablePropss<C, D>
+) => {
+  const { data, columns } = props;
+  const tableData = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+  return (
+    <Box as="table" width="full">
+      <thead>
+        {tableData.getHeaderGroups().map((group) => (
+          <Box as="tr" key={group.id} borderBottom="1px solid #eee">
+            {group.headers.map((header) => {
+              const colHeader = header?.column?.columnDef?.header?.toString();
+              return (
+                <Box
+                 textAlign="left"
+                  color="#5C5E6E"
+                  fontWeight="400"
+                  fontSize="12px"
+                  padding="6px 0"
+                  key={header.id}
+                  as="th"  
+                >
+                  <Flex alignItems="center" columnGap="18px">
+                    {colHeader === "NAME" && (
+                      <Icon
+                        name="users"
+                        style={{ fill: "none", width: "26px" }}
+                      />
+                    )}
+                    {!header.isPlaceholder &&
+                      flexRender(colHeader, header.getContext())}
+                  </Flex>
+                </Box>
+              );
+            })}
+          </Box>
+        ))}
+      </thead>
+
+      <tbody>
+        {tableData.getRowModel().rows.map((row, index) => (
+          <Box
+           borderTop="1px solid rgba(202, 206, 225, 0.4)"
+            as="tr"
+            key={row.id}
+           
+          >
+            {row.getVisibleCells().map((cell) => {
+              return (
+                <Box
+                  padding="15px 0"
+                  color="#292B34"
+                  fontSize="14px"
+                  as="td"
+                  key={cell.id}
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </Box>
+              );
+            })}
+          </Box>
+        ))}
+      </tbody>
+    </Box>
+  );
+};
+
+export default Table;
